cb32e4537666c15036e99d60a913c22211144149
Jason Feuerstein <jwfeuers@usc.edu>
2020-06-14 18:02:24 -0700

    changed background

--- client/src/clients.js
+++ client/src/clients.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import axios from "axios";
-import './App.css';
-import { Container } from "semantic-ui-react";
+import "./App.css";
+import abeLogo from "./abeLogo.png";
 
 let endpoint = "http://localhost:8080";
 
-class Clients extends Component{
-  constructor(props){
-    super (props);
+class Clients extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
       FirstName: "",
       LastName: "",
@@ -17,126 +17,137 @@ class Clients extends Component{
       StateOfIssue: "",
     };
   }
-  handleChange = (event) =>{
+  handleChange = (event) => {
     this.setState({
       [event.target.name]: event.target.value,
     });
-  }
+  };
 
-  onSubmit = () =>{
-    
-    const { FirstName, LastName, PhoneNumber, EmailAddress, Description, StateOfIssue, FindHow, SocialMedia } = this.state;
-    axios.post(endpoint + "/client/api/client",
-      {
-        FirstName: FirstName,
-        LastName: LastName,
-        PhoneNumber: PhoneNumber,
-        EmailAddress: EmailAddress,
-        Description: Description,
-        StateOfIssue: StateOfIssue,
-        findHow: FindHow,
-        socialMedia: SocialMedia
-      },
-      {
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      }
-    }
-    ).then(res => console.log(FirstName));
-    };
-  
-  
+  onSubmit = () => {
+    const {
+      FirstName,
+      LastName,
+      PhoneNumber,
+      EmailAddress,
+      Description,
+      StateOfIssue,
+      FindHow,
+      SocialMedia,
+    } = this.state;
+    axios
+      .post(
+        endpoint + "/api/client",
+        {
+          FirstName: FirstName,
+          LastName: LastName,
+          PhoneNumber: PhoneNumber,
+          EmailAddress: EmailAddress,
+          Description: Description,
+          StateOfIssue: StateOfIssue,
+          findHow: FindHow,
+          socialMedia: SocialMedia,
+        },
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      )
+      .then((res) => console.log(FirstName));
+  };
+
   render() {
-    
-    const { FirstName, LastName, PhoneNumber, EmailAddress, Description, StateOfIssue, FindHow, SocialMedia } = this.state;
+    const {
+      FirstName,
+      LastName,
+      PhoneNumber,
+      EmailAddress,
+      Description,
+      StateOfIssue,
+      FindHow,
+      SocialMedia,
+    } = this.state;
     return (
-      <Container>
+      <div>
+        <img src={abeLogo} className="logo" alt="logo"></img>
         <div className="App">
-            <div className="container" id="registration-form">
-                <div className="image"></div>
-                <div className="frm">
-                    <h1>Case Form</h1>
-              <form onSubmit={this.onSubmit}>
+          <div className="container" id="registration-form">
+            <div className="image"></div>
+
+            <div className="frm">
+              <h1>Case Form</h1>
+              <form className="containerWithoutTitle" onSubmit={this.onSubmit}>
                 <div class="form-group">
                   <h5>First Name:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter first name"
-                        name= "FirstName"
-                        id="firstName"
-                        onChange={this.handleChange}
-                        value = {FirstName || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter first name"
+                      name="FirstName"
+                      id="firstName"
+                      onChange={this.handleChange}
+                      value={FirstName || ""}
                     />
                   </div>
                 </div>
-
 
                 <div class="form-group">
                   <h5>Last Name:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter last name"
-                        name="LastName"
-                        id="lastName"
-                        onChange={this.handleChange}
-                        value = {LastName || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter last name"
+                      name="LastName"
+                      id="lastName"
+                      onChange={this.handleChange}
+                      value={LastName || ""}
                     />
                   </div>
                 </div>
-
 
                 <div class="form-group">
                   <h5>Email:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter email"
-                        name="EmailAddress"
-                        id="emailAddress"
-                        onChange={this.handleChange}
-                        value = {EmailAddress || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter email"
+                      name="EmailAddress"
+                      id="emailAddress"
+                      onChange={this.handleChange}
+                      value={EmailAddress || ""}
                     />
                   </div>
                 </div>
-
 
                 <div class="form-group">
                   <h5>Phone Number:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter phone number"
-                        name="PhoneNumber"
-                        id="phoneNumber"
-                        onChange={this.handleChange}
-                        value = {PhoneNumber || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter phone number"
+                      name="PhoneNumber"
+                      id="phoneNumber"
+                      onChange={this.handleChange}
+                      value={PhoneNumber || ""}
                     />
                   </div>
                 </div>
-
 
                 <div class="form-group">
                   <h5>Location of Legal Issue:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter location of legal issue"
-                        name="StateOfIssue"
-                        id="locationOfLegalIssue"
-                        onChange={this.handleChange}
-                        value = {StateOfIssue || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter location of legal issue"
+                      name="StateOfIssue"
+                      id="locationOfLegalIssue"
+                      onChange={this.handleChange}
+                      value={StateOfIssue || ""}
                     />
                   </div>
                 </div>
@@ -145,14 +156,13 @@ class Clients extends Component{
                   <h5>Description:</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter description of legal issue"
-                        name="Description"
-                        id="Description"
-                        onChange={this.handleChange}
-                        value = {Description || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter description of legal issue"
+                      name="Description"
+                      id="Description"
+                      onChange={this.handleChange}
+                      value={Description || ""}
                     />
                   </div>
                 </div>
@@ -161,30 +171,28 @@ class Clients extends Component{
                   <h5>How did you find Abe Legal?</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder=""
-                        name="FindHow"
-                        id="FindHow"
-                        onChange={this.handleChange}
-                        value = {FindHow || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter the way you find Abe Legal"
+                      name="FindHow"
+                      id="FindHow"
+                      onChange={this.handleChange}
+                      value={FindHow || ""}
                     />
                   </div>
                 </div>
 
                 <div class="form-group">
-                  <h5>Share your Social Media: (Instagram, Twitter)</h5>
+                  <h5>Share your Social Media: (Instagram, Twitter, etc)</h5>
                   <div>
                     <input
-                        type="text"
-                        class="form-control"
-                        placeholder=""
-                        name="SocialMedia"
-                        id="SocialMedia"
-                        onChange={this.handleChange}
-                        value = {SocialMedia || ''}
-
+                      type="text"
+                      class="form-control"
+                      placeholder="Enter your social media ID "
+                      name="SocialMedia"
+                      id="SocialMedia"
+                      onChange={this.handleChange}
+                      value={SocialMedia || ""}
                     />
                   </div>
                 </div>
@@ -195,14 +203,12 @@ class Clients extends Component{
                   </button>
                 </div>
               </form>
-                </div>
             </div>
+          </div>
         </div>
-        </Container>
+      </div>
     );
   }
 }
-  
- 
- 
+
 export default Clients;
